24f94510599b4f155b79dd1436a68819482c3263
dedbob <lonely_noobx@mail.ru>
2017-01-15 16:55:13 +0600

    Create MainActivity class and layout

---
+++ app/src/main/java/com/danielgl/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.danielgl.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dedbob5 on 15.01.2017.
+ */
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
